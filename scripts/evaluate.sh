12da1e16fcd804e39db6f9ec107b094dc7b62ad3
namphuong11 <namphuong.sec.job@gmail.com>
2025-04-25 04:43:31 +0700

    3

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -4,6 +4,6 @@ test_dataset="./dataset/${dataset_name}-cleaned-Test.txt"
 output_path="./generate/1000000/"
 
 # 1. evaluate generated passwords in the normal method
-python evaluate.py --test_file="$test_dataset" --gen_path="$gen_path" --isNormal
+python3.8 evaluate.py --test_file="$test_dataset" --gen_path="$gen_path" --isNormal
 # 2. evaluate generated passwords in the DC-GEN method
-python evaluate.py --test_file="$test_dataset" --gen_path="$gen_path"
+python3.8 evaluate.py --test_file="$test_dataset" --gen_path="$gen_path"
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,8 +4,8 @@ model_path="./model/last-step/"
 output_path="./generate/"
 
 # 1. Get patterns rate
-python get_pattern_rate.py --dataset_path=$cleaned_dataset
+python3.8 get_pattern_rate.py --dataset_path=$cleaned_dataset
 # 2. Generate (use DC-GEN) by using no.4 and no.5 gpus (ids of gpus should be continuous)
 # python DC-GEN.py --model_path=$model_path --output_path=$output_path --generate_num=1000000 --batch_size=5000 --gpu_num=2 --gpu_index=4
 # 3. or Generate (not use DC-GEN) 
-python normal-gen.py --model_path=$model_path --output_path=$output_path --generate_num=1000000 --batch_size=5000 --gpu_num=2 --gpu_index=4
+python3.8 normal-gen.py --model_path=$model_path --output_path=$output_path --generate_num=1000000 --batch_size=5000 --gpu_num=2 --gpu_index=4
